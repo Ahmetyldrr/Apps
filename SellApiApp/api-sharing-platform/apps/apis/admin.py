f6a907a4c160d718557e03e2042e5124187824d2
Ahmet Yıldırır <60220232+Ahmetyldrr@users.noreply.github.com>
2025-10-15 06:10:57 +0300

    Delete SellApiApp directory

--- SellApiApp/api-sharing-platform/apps/apis/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Api
-
-class ApiAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'created_at', 'updated_at')
-    search_fields = ('name', 'description')
-
-admin.site.register(Api, ApiAdmin)
