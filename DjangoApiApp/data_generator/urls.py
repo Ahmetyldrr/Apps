744ec776a79e0e44516907ba58dacb1d62948111
Ahmet Yıldırır <60220232+Ahmetyldrr@users.noreply.github.com>
2025-10-15 06:09:54 +0300

    Delete DjangoApiApp directory

--- DjangoApiApp/data_generator/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import GenerateForecastView, ForecastDashboardView, DownloadForecastView
-
-urlpatterns = [
-    path('', ForecastDashboardView.as_view(), name='forecast-dashboard'),
-    path('generate-forecast/', GenerateForecastView.as_view(), name='generate-forecast'),
-    path('download-forecast/', DownloadForecastView.as_view(), name='download-forecast'),
-]
